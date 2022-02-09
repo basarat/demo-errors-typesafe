9ca5f069ba892ad0be3b1bf46fe97a399d9eaf2b
basarat <basaratali@gmail.com>
2022-02-09 11:48:25 +1100

    wrap

---
+++ src/index.ts
@@ -0,0 +1,25 @@
+import { emailValidator, save, notifyUser } from './vendor';
+
+export type Validator = (value: string) => boolean;
+
+type ValidateResult = { value: string } | { error: string }
+
+/** A general purpose validate function */
+const validate = (value: string, validators: Validator[]): ValidateResult => {
+  const isValid = validators.some(validator => validator(value) === false);
+  if (isValid) {
+    return { value };
+  } else {
+    return { error: 'Invalid value' };
+  }
+}
+
+/** If valid, save, else notify */
+function submit(email: string) {
+  const result = validate(email, [emailValidator]);
+  if ('error' in result) {
+    notifyUser(result.error);
+    return;
+  } 
+  save(result.value);
+}
---
+++ src/vendor.ts
@@ -0,0 +1,10 @@
+
+export const emailValidator = (value: string) => {
+  return !!value.toLowerCase()
+    .match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    );
+};
+
+export const save = (value: any) => null;
+export const notifyUser = (value: any) => null;
